089fe65cd42e5b9ed3a3037e0289c43a2a6886f7
june0216 <ziyun1612@ewhain.net>
2023-05-16 15:19:11 +0900

    BOJ : 누적합

--- BOJ/구현/25827.py
+++ BOJ/구현/25827.py
@@ -1,24 +1,26 @@
 import sys
+
 input = sys.stdin.readline
-n = int(input())
 
-h = 3600
-m = 60
-table = [0] * (h * 24 + m * 60 + 60)  # [00:00:00, 23:59:59]의 각 초를 저장할 리스트 초기화
-
-for _ in range(n):
-    q = list(input().split())
-    start_list = list(map(int, q[1].split(":")))
-    end_list = list(map(int, q[2].split(":")))
-    start = start_list[0] * h + start_list[1] * m + start_list[2]
-    end = end_list[0] * h + end_list[1] * m + end_list[2]
+N = int(input())
+prefix_sum = [0] * (86400 + 1) # [00:00:00, 23:59:59]의 각 초를 저장할 리스트 (24시간 * 60분 * 60초 + 1) == (전체 시간 + 누적합을 위해 )
+prefix_sum[0] = -1
+while N:
+    q = input().split()
 
     if q[0] == '1':
-        table[start] += 1
-        table[end] -= 1
+        start = int(q[1][0:2]) * 3600 + int(q[1][3:5]) * 60 + int(q[1][6:8])
+        end = int(q[2][0:2]) * 3600 + int(q[2][3:5]) * 60 + int(q[2][6:8])
+        prefix_sum[start + 1] += 1
+        prefix_sum[end + 1] -= 1
     else:
-        for i in range(1, len(table)):
-            table[i] = table[i] + table[i - 1]
-        print(sum(table[start:end]))
-        for i in range(len(table) - 1, 0, -1):
-            table[i] -= table[i - 1]
+        start = int(q[1][0:2]) * 3600 + int(q[1][3:5]) * 60 + int(q[1][6:8])
+        end = int(q[2][0:2]) * 3600 + int(q[2][3:5]) * 60 + int(q[2][6:8])
+        if prefix_sum[0] == -1:
+            prefix_sum[0] = 0
+            for _ in range(2):
+                for i in range(1, len(prefix_sum)):
+                    prefix_sum[i] += prefix_sum[i - 1]
+        ans = prefix_sum[end] - prefix_sum[start]
+        print(ans)
+    N -= 1
