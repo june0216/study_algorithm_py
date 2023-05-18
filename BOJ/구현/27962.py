8a0cad2849d658a02d9cc40e606a5024998f40eb
june0216 <ziyun1612@ewhain.net>
2023-05-18 20:40:08 +0900

    BOJ : 완탐

--- BOJ/구현/27962.py
+++ BOJ/구현/27962.py
@@ -1,15 +1,23 @@
 import sys
+
 n = int(input())
 s = input().strip()
 
+if n == 1:
+    print("NO")
+    sys.exit(0)
 
-for i in range(n-3):
-    window1 = s[i:i+3]
-
-    for k in range(i+1, n-3):
-        window2 = s[k:k+3]
-        if window1[0] == window2[0] and window1[-1] == window2[-1]:
-            
+for i in range(1, n):
+    window1 = s[:i]
+    l = len(window1)
+    window2 = s[n-l:n]
+    if window2[0] == window1[0] or window1[-1] == window2[-1]:
+        diff = 0
+        for j in range(l):
+            if window1[j] != window2[j]:
+                diff += 1
+            # 정확히 한 문자만 다르다면 비타민 문자열
+        if diff == 1:
             print("YES")
             sys.exit(0)
 
