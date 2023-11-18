51e3c8fcc92477359c0096fe0eea6e8cbc5be4c6
kwonjiyun <ziyun1612@ewhain.net>
2023-11-18 13:37:39 +0900

    백준 : DP

--- BOJ/DP/14501.py
+++ BOJ/DP/14501.py
@@ -2,10 +2,8 @@ N = int(input())
 li = [list(map(int, input().split())) for _ in range(N)]
 dp = [0 for _ in range(N+1)]
 
-for i in range(N-1, -1, -1):
-    if i + li[i][0] > N:
-        dp[i] = dp[i+1]
-    else:
-        dp[i] = max(dp[i+1], li[i][1] + dp[i + li[i][0]])
-
-print(dp[0])
+for i in range(N):
+    for j in range(li[i][0]+i, N+1):
+        if dp[j] < dp[i] + li[i][1]:
+            dp[j] = dp[i] + li[i][1]
+print(dp[-1])
